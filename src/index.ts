14a5974d9518193a0045c42e65698f5533aeb1c1
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2023-03-22 22:13:46 +0530

    Using web.crypto-subtle based generation strategy with tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export const hello = () => {
-    return 'Hello World!';
-}
+import * as crypto from 'crypto';
+
+export const generateTOTP = async (secret: string): Promise<string> => {
+    const time = Math.floor(Date.now() / 1000); // get the current time in seconds
+    const timeWindow = 30; // time window in seconds
+    const timeStep = Math.floor(time / timeWindow); // calculate the time step
+    const counter = new Uint8Array(8); // counter as an 8-byte array
+    counter[7] = timeStep & 0xff; // populate the least significant byte of the counter
+    counter[6] = (timeStep >> 8) & 0xff;
+    counter[5] = (timeStep >> 16) & 0xff;
+    counter[4] = (timeStep >> 24) & 0xff;
+    const hmacKey = await crypto.subtle.importKey(
+        'raw', // format of the secret key
+        new TextEncoder().encode(secret), // the secret key as a byte array
+        { name: 'HMAC', hash: { name: 'SHA-1' } }, // algorithm to use for HMAC
+        false, // not extractable
+        ['sign'] // only need the key for signing
+    );
+    const hmacResult = await crypto.subtle.sign(
+        'HMAC', // HMAC algorithm
+        hmacKey,
+        counter // the counter as data to sign
+    );
+    const hmacArray = new Uint8Array(hmacResult); // convert the result to a byte array
+    const offset = hmacArray[hmacArray.length - 1] & 0xf; // get the offset
+    const code =
+        ((hmacArray[offset] & 0x7f) << 24) |
+        ((hmacArray[offset + 1] & 0xff) << 16) |
+        ((hmacArray[offset + 2] & 0xff) << 8) |
+        (hmacArray[offset + 3] & 0xff); // calculate the code
+     // format the code as a 6-digit string
+    return (code % 10 ** 6).toString().padStart(6, '0');
+};
+
+export const verifyTOTP = async (secret: string, token: string): Promise<boolean> => {
+    const otp = await generateTOTP(secret); // generate the expected OTP
+    return otp === token; // compare the expected OTP with the user-entered token
+};
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,22 @@
 import {describe, it, expect} from "vitest";
-import {hello} from "../src";
+import {verifyTOTP, generateTOTP} from "../src";
 
-describe("test", async () => {
-    it("should work", async () => {
-        expect(hello()).toBe("Hello World!")
+describe("TOTP generation should work by specs", async () => {
+    it("should generate a valid OTP", async () => {
+        const secret = 'mysecret';
+        const otp = await generateTOTP(secret);
+        expect(otp).toMatch(/^\d{6}$/); // OTP should be a 6-digit string
     })
+    it('should verify a valid OTP', async () => {
+        const secret = 'mysecret';
+        const otp = await generateTOTP(secret);
+        const result = await verifyTOTP(secret, otp);
+        expect(result).toBe(true);
+    });
+    it('should reject an invalid OTP', async () => {
+        const secret = 'mysecret';
+        const otp = '123456';
+        const result = await verifyTOTP(secret, otp);
+        expect(result).toBe(false);
+    });
 })
